269fd0e8682ee88cfadd903daa786c43212b7061
Tristan Hill <tristan@localhost>
2026-02-06 14:04:26 +0000

    more

--- handlers/copy.rb
+++ handlers/copy.rb
@@ -19,6 +19,8 @@ module DataPipeline
         Rollbar.error(e)
       end
 
+      private
+
       def copy(from, to)
         @client.copy_object(bucket: bucket_name(:process), copy_source: URI::DEFAULT_PARSER.escape(from), key: to)
         logger.info("Copied to #{copy_key}")
