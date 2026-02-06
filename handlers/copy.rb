d0a7f0fc2bcf688ceabc4938cfc193367ea17f16
Tristan Hill <tristan@localhost>
2026-02-06 14:03:47 +0000

    more

--- handlers/copy.rb
+++ handlers/copy.rb
@@ -8,10 +8,20 @@ module DataPipeline
     class Copy < HandlerBase
       def process(key:, bucket:)
         _, identifier, file = key.split('/', 3)
-        response = @client.copy_object(bucket: bucket_name(:process),
-                                       copy_source: URI::DEFAULT_PARSER.escape("#{bucket}/#{key}"),
-                                       key: "#{identifier}/#{prefix_timestamp}/#{file}")
-        respond 200, response
+        if identifier.nil? || file.nil?
+          logger.info("Invalid key for #{key}")
+          return
+        end
+        copy("#{bucket}/#{key}", "#{identifier}/#{prefix_timestamp}/#{file}")
+      rescue StandardError => e
+        logger.error("Unable to copy: #{key}")
+        logger.error(e.inspect)
+        Rollbar.error(e)
+      end
+
+      def copy(from, to)
+        @client.copy_object(bucket: bucket_name(:process), copy_source: URI::DEFAULT_PARSER.escape(from), key: to)
+        logger.info("Copied to #{copy_key}")
       end
     end
   end
